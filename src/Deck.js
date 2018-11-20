a46f95de429821182ede71f40a34982b8471a31f
Vadym <VadymSemchenko>
2018-11-20 19:28:55 +0200

    add rotation interpolationg to dimensions

--- src/Deck.js
+++ src/Deck.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
-import { View, Animated, PanResponder } from 'react-native';
+import {
+    View,
+    Animated,
+    PanResponder,
+    Dimensions
+} from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 class Deck extends Component {
 
-    position = new Animated.ValueXY();
+    position = new Animated.ValueXY(0,0);
     panResponder = PanResponder.create({
         onStartShouldSetPanResponder: () => true,
         onPanResponderMove: (event, gesture) => {
-            console.log('gesture', gesture);
             this.position.setValue({
                 x: gesture.dx,
                 y: gesture.dy
@@ -16,12 +22,25 @@ class Deck extends Component {
         onPanResponderRelease: () => {}
     });
 
+    getCardStyle() {
+        const rotate = this.position.x.interpolate({
+            inputRange: [-SCREEN_WIDTH * 1.5, 0, +SCREEN_WIDTH * 1.5],
+            outputRange: ['-120deg', '0deg', '120deg']
+        });
+        const cardStyle = {
+            ...this.position.getLayout(),
+            transform: [{
+                rotate
+            }]
+        };
+        console.log('CARD STYLE', cardStyle);
+        return cardStyle;
+    };
 
     renderCards = () => this.props.data.map((item, index) => {
             if (index === 0) {
-                console.log('FIRST ONE');
                 return (
-                    <Animated.View key={item.text} style={this.position.getLayout()} {...this.panResponder.panHandlers}>
+                    <Animated.View key={item.text} style={this.getCardStyle()} {...this.panResponder.panHandlers}>
                         {this.props.renderCard(item)}
                     </Animated.View>
                 );
@@ -30,6 +49,7 @@ class Deck extends Component {
         });
 
     render() {
+        console.log('RENDER');
         return (
             <View>
                 {this.renderCards()}
