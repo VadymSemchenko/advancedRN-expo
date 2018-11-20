13d9dcb7929d2f12f9110ee01a250508b969d295
Vadym <VadymSemchenko>
2018-11-20 14:21:42 +0200

    solve problems with expo reloading

--- App.js
+++ App.js
@@ -1,14 +1,44 @@
-import React from 'react';
+import Expo from 'expo';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+
+import Deck from './src/Deck';
 
 console.disableYellowBox = true;
 
-export default class App extends React.Component {
+console.log('START');
+
+const DATA = [
+  { id: 1, text: 'Card #1', uri: 'https://vignette.wikia.nocookie.net/dragonballfanon/images/7/70/Random.png/revision/latest?cb=20161221030547' },
+  { id: 2, text: 'Card #2', uri: 'https://vignette.wikia.nocookie.net/dragonballfanon/images/7/70/Random.png/revision/latest?cb=20161221030547' },
+  { id: 3, text: 'Card #3', uri: 'https://vignette.wikia.nocookie.net/dragonballfanon/images/7/70/Random.png/revision/latest?cb=20161221030547' },
+  { id: 4, text: 'Card #4', uri: 'https://vignette.wikia.nocookie.net/dragonballfanon/images/7/70/Random.png/revision/latest?cb=20161221030547' },
+  { id: 5, text: 'Card #5', uri: 'https://vignette.wikia.nocookie.net/dragonballfanon/images/7/70/Random.png/revision/latest?cb=20161221030547' },
+  { id: 6, text: 'Card #6', uri: 'https://vignette.wikia.nocookie.net/dragonballfanon/images/7/70/Random.png/revision/latest?cb=20161221030547' },
+  { id: 7, text: 'Card #7', uri: 'https://vignette.wikia.nocookie.net/dragonballfanon/images/7/70/Random.png/revision/latest?cb=20161221030547' },
+  { id: 8, text: 'Card #8', uri: 'https://vignette.wikia.nocookie.net/dragonballfanon/images/7/70/Random.png/revision/latest?cb=20161221030547' },
+  { id: 9, text: 'Card #9', uri: 'https://vignette.wikia.nocookie.net/dragonballfanon/images/7/70/Random.png/revision/latest?cb=20161221030547' }
+];
+
+export default class App extends Component {
+
+  renderCard(item) {
+    console.log('TEXT', item.text);
+    return (
+        <Text key={item.text} >
+          {item.text}
+        </Text>
+    );
+  };
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start workin on your app!</Text>
-      </View>
+        <View style={styles.container}>
+          <Deck
+            data={DATA}
+            renderCard={this.renderCard}
+          />
+        </View>
     );
   }
 }
@@ -17,7 +47,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center'
   },
 });
